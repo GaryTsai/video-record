ce930ef14f57af4b43ae830f185a0718efab8101
gary_tsai <gary_tsai@perfectcorp.com>
2021-02-18 01:13:10 +0800

    Updates

---
+++ precache-manifest.0f8ac05c4279448cf118e314ac484cd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b76b38aca16d67b864c5b63fac57d378",
+    "url": "/video-record/index.html"
+  },
+  {
+    "revision": "278d8439a6bc71be85c2",
+    "url": "/video-record/static/css/main.085bb5ee.chunk.css"
+  },
+  {
+    "revision": "dcace55b01c710c6799b",
+    "url": "/video-record/static/js/2.75b6b469.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/video-record/static/js/2.75b6b469.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4624ec164c858a43b226",
+    "url": "/video-record/static/js/3.d45be9fa.chunk.js"
+  },
+  {
+    "revision": "278d8439a6bc71be85c2",
+    "url": "/video-record/static/js/main.1527be54.chunk.js"
+  },
+  {
+    "revision": "b3a998e42e8279edfe05",
+    "url": "/video-record/static/js/runtime-main.79cda88e.js"
+  }
+]);
